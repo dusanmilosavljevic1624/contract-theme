af635207f445d0667e1a8cf34e0b425059e14344
gaurav <g.manhas@gmail.com/gsmanhas>
2016-04-21 22:19:08 +0200

    Navigation drop down menu

--- app/includes/navigation/navigation-callback.js
+++ app/includes/navigation/navigation-callback.js
@@ -1,5 +1,14 @@
 define(["jquery"], function ($) {
     return function () {
-        console.log("Navigation JS Callback executed")
+        console.log("Navigation JS Callback executed");
+
+        $('.toggle-dropdown').hide();
+
+        $('.toggle-menu').on('click', function (e) {
+          e.preventDefault();
+
+          $('.toggle-container').toggleClass('toggled');
+          $('.toggle-dropdown').toggle();
+        })
     }
 });
