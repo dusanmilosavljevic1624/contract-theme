bc412c63bf7ad2f432cfd20a34748e96186034e3
gaurav <g.manhas@gmail.com/gsmanhas>
2016-04-22 18:56:08 +0200

    Adding the dropdown menu animation

--- app/includes/navigation/navigation-callback.js
+++ app/includes/navigation/navigation-callback.js
@@ -1,14 +1,27 @@
-define(["jquery"], function ($) {
-    return function () {
+define(["jquery"], function($) {
+    return function() {
         console.log("Navigation JS Callback executed");
+
 
         $('.toggle-dropdown').hide();
 
-        $('.toggle-menu').on('click', function (e) {
-          e.preventDefault();
+        $('.toggle-menu').on('click', function(e) {
+            e.preventDefault();
 
-          $('.toggle-container').toggleClass('toggled');
-          $('.toggle-dropdown').toggle();
+            $('.toggle-container').toggleClass('toggled');
+            if ($('.toggle-dropdown').css('left') === '0px') {
+                $('.toggle-dropdown').animate({
+                  left: '100%'
+                });
+                setTimeout(function () {
+                  $('.toggle-dropdown').hide();
+                }, 600);
+            } else {
+                $('.toggle-dropdown').show();
+                $('.toggle-dropdown').animate({
+                    left: '0%'
+                }, 500);
+            }
         })
     }
 });
