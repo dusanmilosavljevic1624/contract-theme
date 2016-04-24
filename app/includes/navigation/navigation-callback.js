8102020c84ff364c5f13e3b1f03a470c68fe4286
gaurav <g.manhas@gmail.com/gsmanhas>
2016-04-24 08:09:34 +0200

    Adding changes to responsive navigation

--- app/includes/navigation/navigation-callback.js
+++ app/includes/navigation/navigation-callback.js
@@ -3,12 +3,13 @@ define(["jquery"], function($) {
         console.log("Navigation JS Callback executed");
 
 
+
         $('.toggle-dropdown').hide();
 
         $('.toggle-menu').on('click', function(e) {
             e.preventDefault();
 
-            $('.toggle-container').toggleClass('toggled');
+            $('.toggle-menu').toggleClass('toggled');
             if ($('.toggle-dropdown').css('left') === '0px') {
                 $('.toggle-dropdown').animate({
                   left: '100%'
