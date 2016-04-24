e129073aaba9bd9f8bff6677f7fd1d65e81ff35f
Dusan Milosavljevic <dusan.milosavljevic.1624@gmail.com>
2016-04-24 11:56:28 +0200

    Profile mobile view

--- app/includes/profile/profile-callback.js
+++ app/includes/profile/profile-callback.js
@@ -1,5 +1,31 @@
-define(["jquery"], function ($) {
-    return function () {
-        console.log("Profile JS Callback executed")
+define(["jquery"], function($) {
+    return function() {
+        console.log("Profile JS Callback executed");
+
+
+        function alignButtons() {
+            setTimeout(function() {
+                var profileHeight = $('.profile-image-container img').outerHeight();
+                $('.button-container ul').height(profileHeight);
+                var $buttons = $('#profile .button-container .button');
+                var btnHeight = $buttons.outerHeight();
+                var btnNumber = $buttons.length;
+
+                $buttons.css('margin-bottom', ((profileHeight - (btnHeight * btnNumber)) / 2));
+            }, 500);
+        }
+
+        alignButtons();
+
+        var resizeTimer;
+
+        $(window).on('resize', function(e) {
+            clearTimeout(resizeTimer);
+
+            resizeTimer = setTimeout(function() {
+                alignButtons();
+            }, 250);
+
+        });
     }
 });
